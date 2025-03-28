ecc00d7ca88e35c9b4393ce52f522ee508fe48d5
Fernando-Orozco-2006 <fqop06@gmail.com>
2025-03-28 16:53:30 -0600

    Add files via upload

---
+++ Caminos Mínimos en Grafos/Caminos Mínimos en Grafos/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+class Graph
+{
+    private int[,] adjacencyMatrix;
+    private int vertices;
+    public Graph(int vertices)
+    {
+        this.vertices = vertices;
+        adjacencyMatrix = new int[vertices, vertices];
+        for (int i = 0; i < vertices; i++)
+        {
+            for (int j = 0; j < vertices; j++)
+            {
+                adjacencyMatrix[i, j] = (i == j) ? 0 : int.MaxValue;
+            }
+        }
+    }
+    public void AddEdge(int from, int to, int weight)
+    {
+        adjacencyMatrix[from, to] = weight;
+    }
+    public void PrintMatrix()
+    {
+        for (int i = 0; i < vertices; i++)
+        {
+            for (int j = 0; j < vertices; j++)
+            {
+                if (adjacencyMatrix[i, j] == int.MaxValue)
+                    Console.Write("INF ");
+                else
+                    Console.Write(adjacencyMatrix[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+    public void FloydWarshall()
+    {
+        int[,] dist = (int[,])adjacencyMatrix.Clone();
+        for (int k = 0; k < vertices; k++)
+        {
+            for (int i = 0; i < vertices; i++)
+            {
+                for (int j = 0; j < vertices; j++)
+                {
+                    if (dist[i, k] != int.MaxValue && dist[k, j] != int.MaxValue)
+                    {
+                        dist[i, j] = Math.Min(dist[i, j], dist[i, k] + dist[k, j]);
+                    }
+                }
+            }
+        }
+        adjacencyMatrix = dist;
+        Console.WriteLine("Matriz de distancias más cortas (Floyd-Warshall):");
+        PrintMatrix();
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Favor de ingresar el número de nodos: ");
+        int nodes = int.Parse(Console.ReadLine());
+        Graph graph = new Graph(nodes);
+        Console.Write("Favor de ingresar el de aristas: ");
+        int edges = int.Parse(Console.ReadLine());
+        for (int i = 0; i < edges; i++)
+        {
+            Console.Write("Favor de ingresar el origen, destino y peso: ");
+            string[] input = Console.ReadLine().Split();
+            if (input.Length < 3)
+            {
+                Console.WriteLine("Entrada inválida. Debe ingresar tres valores: origen, destino y peso.");
+                i--;
+                continue;
+            }
+            if (!int.TryParse(input[0], out int from) || !int.TryParse(input[1], out int to) || !int.TryParse(input[2], out int weight))
+            {
+                Console.WriteLine("Entrada inválida. Asegúrese de ingresar números enteros.");
+                i--;
+                continue;
+            }
+            graph.AddEdge(from, to, weight);
+        }
+        Console.WriteLine("Matriz de adyacencia:");
+        graph.PrintMatrix();
+        Console.WriteLine("Ejecutando Floyd-Warshall...");
+        graph.FloydWarshall();
+    }
+}
+
---
+++ Caminos Mínimos en Grafos/Caminos Mínimos en Grafos/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Caminos Mínimos en Grafos")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Caminos Mínimos en Grafos")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6c57e28c-4212-4322-8321-5888f604d4d3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
